bd92690519f0d5b4e0e765aa9d857eaa6cbee0e4
oleg <zmey3854gmail.com>
2024-11-25 11:07:27 +0300

    task 3.1

---
+++ bin/gendiff.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import fs from 'fs';
+import gendiff from '../index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2))
+  })
+
+program.parse();
+// console.log(program)
---
+++ index.js
@@ -0,0 +1,7 @@
+// const gendiff = (filepath1, filepath2) => {
+//     return [path.resolve(process.cwd(), filepath1),
+//         path.resolve(process.cwd(), filepath2)]
+
+//     }
+  
+// export default gendiff;
