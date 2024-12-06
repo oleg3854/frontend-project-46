637eae22d1c196ea33f0c14ce53297c8b3ffd94d
oleg <zmey3854gmail.com>
2024-12-06 15:01:11 +0300

    task2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,5 +2,7 @@
 import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
