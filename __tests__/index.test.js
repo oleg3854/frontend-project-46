242532d972043ae5385e2b0e3bfa1366e44317ad
oleg <zmey3854gmail.com>
2024-12-06 17:44:33 +0300

    task 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,16 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
+
 test('basic', () => {
-  const file1 = '__fixtures__/file1.json';
-  const file2 = '__fixtures__/file2.json';
+  const file1 = 'file1.json';
+  const file2 = 'file2.json';
   const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
-  expect(genDiff(file1, file2)).toEqual(result);
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2))).toEqual(result);
 });
